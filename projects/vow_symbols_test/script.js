d3a252e731ea42bafcc9fd76f6afa485a31a72af
Actatus <itsactatus@yahoo.com>
2022-07-24 14:22:45 -0700

    Checks for double randoms.

--- projects/vow_symbols_test/script.js
+++ projects/vow_symbols_test/script.js
@@ -13,89 +13,117 @@ document.addEventListener('DOMContentLoaded', function () {
 const symbolsObject = {
     "black garden" : {
         symbolName : "black garden",
-        imageLocation : "assets/vowSymbols/black_garden.png"
+        imageLocation : "assets/vowSymbols/black_garden.png",
+        alt: "A symbol of a black plateau sitting on top of a red plane. A box with a transparent center floats in the sky."
     }, "black heart" : {
         symbolName : "black heart",
-        imageLocation : "assets/vowSymbols/black_heart.png"
+        imageLocation : "assets/vowSymbols/black_heart.png",
+        alt: "A dark mass sitting inside a dark horseshoe surrounded by a circle/band."
     }, "commune" : {
         symbolName : "commune",
-        imageLocation : "assets/vowSymbols/commune.png"
+        imageLocation : "assets/vowSymbols/commune.png",
+        alt: "A dark pyramid with green rays shooting out from it."
     }, "dark" : {
         symbolName : "dark",
-        imageLocation: "assets/vowSymbols/dark.png"
+        imageLocation: "assets/vowSymbols/dark.png",
+        alt: "A dark circle with white lines moving out from it."
     }, "drink" : {
         symbolName : "drink",
-        imageLocation: "assets/vowSymbols/drink.png"
+        imageLocation: "assets/vowSymbols/drink.png",
+        alt: "A person with a blue liquid pouring into it."
     }, "earth" : {
         symbolName : "earth",
-        imageLocation: "assets/vowSymbols/earth.png"
+        imageLocation: "assets/vowSymbols/earth.png",
+        alt: "A blue sphere with a green mass inside it. A white orb floats above the blue sphere."
     }, "enter" : {
         symbolName : "enter",
-        imageLocation: "assets/vowSymbols/enter.png"
+        imageLocation: "assets/vowSymbols/enter.png",
+        alt: "A white trapezoid with a triangle sitting inside it. A red semi-circle spills out from underneath."
     }, "fleet" : {
         symbolName : "fleet",
-        imageLocation: "assets/vowSymbols/fleet.png"
+        imageLocation: "assets/vowSymbols/fleet.png",
+        alt: "5 pyramids sitting in fron of a bronze sphere."
     }, "give" : {
         symbolName : "give",
-        imageLocation: "assets/vowSymbols/give.png"
+        imageLocation: "assets/vowSymbols/give.png",
+        alt: "Two hands push a downard-facing pyramid away from them."
     }, "grief" : {
         symbolName : "grief",
-        imageLocation: "assets/vowSymbols/grief.png"
+        imageLocation: "assets/vowSymbols/grief.png",
+        alt: "A white humanoid with a black sphere in its chest."
     }, "guardian" : {
         symbolName : "guardian",
-        imageLocation: "assets/vowSymbols/guardian.png"
+        imageLocation: "assets/vowSymbols/guardian.png", 
+        alt: "A person with a Destiny 2 Ghost floating above them."
     }, "hive" : {
         symbolName : "hive",
-        imageLocation: "assets/vowSymbols/hive.png"
+        imageLocation: "assets/vowSymbols/hive.png",
+        alt: "A Destiny 2 Hive figure with 3 glowing eyes."
     }, "kill" : {
         symbolName : "kill",
-        imageLocation: "assets/vowSymbols/kill.png"
+        imageLocation: "assets/vowSymbols/kill.png",
+        alt: "A red sphere with a white triangle piercing it."
     }, "knowledge" : {
         symbolName : "knowledge",
-        imageLocation: "assets/vowSymbols/knowledge.png"
+        imageLocation: "assets/vowSymbols/knowledge.png",
+        alt : "A brain divided into multiple sections."
     }, "light" : {
         symbolName : "light",
-        imageLocation: "assets/vowSymbols/light.png"
+        imageLocation: "assets/vowSymbols/light.png",
+         alt: "A white sphere surrounded by multiple rays of various colors."
     }, "love" : {
         symbolName : "love",
-        imageLocation: "assets/vowSymbols/love.png"
+        imageLocation: "assets/vowSymbols/love.png",
+        alt : "Four hearts surrounding a spiral."
     }, "neutral" : {
         symbolName : "neutral",
-        imageLocation: "assets/vowSymbols/neutral.png"
+        imageLocation: "assets/vowSymbols/neutral.png",
+        alt : "A grey circle or band surrounding a black sphere."
     }, "plane" : {
         symbolName : "plane",
-        imageLocation: "assets/vowSymbols/plane.png"
+        imageLocation: "assets/vowSymbols/plane.png",
+        alt : "A white half-circle with a mirrored dark half-circle under it."
     }, "pyramid" : {
         symbolName : "pyramid",
-        imageLocation: "assets/vowSymbols/pyramid.png"
+        imageLocation: "assets/vowSymbols/pyramid.png",
+        alt : "A black triangle pointing upwards."
     }, "savathun" : {
         symbolName : "savathun",
-        imageLocation: "assets/vowSymbols/savathun.png"
+        imageLocation: "assets/vowSymbols/savathun.png",
+        alt : "The Witch Queen logo"
     }, "scorn" : {
         symbolName : "scorn",
-        imageLocation: "assets/vowSymbols/scorn.png"
+        imageLocation: "assets/vowSymbols/scorn.png",
+        alt : "A brown F on the right is mirrored with a reversed F on the left with a vertical line between them."
     }, "stop" : {
         symbolName : "stop",
-        imageLocation: "assets/vowSymbols/stop.png"
+        imageLocation: "assets/vowSymbols/stop.png",
+        alt : "A red square."
     }, "tower" : {
         symbolName : "tower",
-        imageLocation: "assets/vowSymbols/tower.png"
+        imageLocation: "assets/vowSymbols/tower.png",
+        alt : "A banner sits in front of a white sphere."
     }, "traveler" : {
         symbolName : "traveler",
-        imageLocation: "assets/vowSymbols/traveler.png"
+        imageLocation: "assets/vowSymbols/traveler.png",
+        alt : "A white sphere sits above a blue half sphere. The blue half spehre is cut off by the bottom of the symbol."
     }, "witness" : {
         symbolName : "witness",
-        imageLocation: "assets/vowSymbols/witness.png"
+        imageLocation: "assets/vowSymbols/witness.png",
+        alt : "A dark figure/person with a tan gaseous cloud coming from their head."
     }, "worm" : {
         symbolName : "worm",
-        imageLocation: "assets/vowSymbols/worm.png"
+        imageLocation: "assets/vowSymbols/worm.png",
+        alt : "A serpent coils around a black blob."
     }, "worship" : {
         symbolName : "worship",
-        imageLocation: "assets/vowSymbols/worship.png"
+        imageLocation: "assets/vowSymbols/worship.png",
+        alt : "A small tan figure stretches its arms out sitting in front of a dark figure."
     }
 }
 
 let answer = '';
+let prevAnswerIndex = '';
 let score = 0;
 let timer = 0;
 let responseDifficulty = 4;
@@ -105,6 +133,8 @@ const outputContainer = document.getElementById('symbolOutputContainer');
 const guardianDownSound = document.getElementsByTagName('audio')[0];
 const scoreOutput = document.getElementById('score');
 const timerOutput = document.getElementById('timer');
+const tester = document.getElementById('tester');
+
 
 const shakeAnimation = [
     {transform: 'translate(5px, 5px) rotate(0deg)'},
@@ -129,6 +159,11 @@ function runTest(){
 function randomizeSymbol(){
     //Use a random number to find a random key from Symbols list. Pass that symbol and an index down.
     let randomIndex = Math.floor(Math.random() * 26)
+    if (randomIndex == prevAnswerIndex){
+        while (randomIndex == prevAnswerIndex){
+            randomIndex = Math.floor(Math.random() * 26);
+        }
+    }
     let randomSymbol = Object.keys(symbolsObject)[randomIndex];
     let returnedObject = symbolsObject[randomSymbol];
     returnedObject.index = randomIndex;
@@ -143,8 +178,9 @@ function outputSymbol(){
 
     let randomizedSymbol = randomizeSymbol();
     symbolImageElement.src = randomizedSymbol.imageLocation;
-    console.log(randomizedSymbol);
+    symbolImageElement.alt = randomizedSymbol.alt;
     answer = randomizedSymbol.symbolName;
+    prevAnswerIndex = randomizedSymbol.index;
 
     randomizeResponses(randomizedSymbol);
     return randomizedSymbol;
@@ -205,7 +241,6 @@ function checkAnswer(selectedResponse){
     if (selectedResponse == answer){
         score++
         scoreOutput.textContent = score;
-       
         clearTimer();
         runTest();
     } else {
